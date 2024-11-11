5355b1c79ff3020b8962dd42712058f892c24ffd
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-11-11 12:26:30 +0300

    Add a forgotten script

---
+++ scripts/plot-min-max.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import pandas as pd
+import sqlite3
+from PIL import Image
+from PIL.ImageOps import invert
+import sys
+import os
+
+f = lambda e: 4*e/(np.pi * (3*(1+e) - np.sqrt((3*e+1) * (e+3))))
+
+what = sys.argv[1]
+how = sys.argv[2]
+mid = float(sys.argv[3])
+start = mid - 0.05
+end   = mid + 0.05
+
+with sqlite3.connect('glyphs.db') as db:
+    general = pd.read_sql_query('select * from pores where lfer > 0.9', db)
+
+shit = pd.read_csv('score.csv')
+data = general.merge(shit, on = 'label', how = 'inner', validate = "1:1")
+data['score'] = data['score'] / f(data['elongation'])
+
+query = 'convexity >= %f and convexity < %f' % (start, end)
+subdata = data.query(query).sort_values(what, axis = 0)
+
+print(subdata)
+
+plt.rc('font', size = 14)
+fig = plt.figure(figsize = (10, 4), dpi = 300)
+for i in range(5):
+    name = subdata.iloc[i]['label'] + '.pbm'
+    img = Image.open(os.path.expanduser(os.path.join('~', 'test', 'grains', name)))
+    ax = fig.add_subplot(2, 5, i+1)
+    ax.imshow(invert(img))
+    ax.title.set_text('$%s = %.4f$' % (how, subdata.iloc[i][what]))
+    ax.xaxis.set_major_locator(ticker.NullLocator())
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+
+for i in range(5):
+    name = subdata.iloc[-i-1]['label'] + '.pbm'
+    img = Image.open(os.path.expanduser(os.path.join('~', 'test', 'grains', name)))
+    ax = fig.add_subplot(2, 5, i+6)
+    ax.imshow(invert(img))
+    ax.title.set_text('$%s = %.2f$' % (how, subdata.iloc[-i-1][what]))
+    ax.xaxis.set_major_locator(ticker.NullLocator())
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+
+plt.subplots_adjust(wspace = 0.3, hspace = 0.05)
+plt.savefig('groups-%s-%.2f.png' % (what, mid), bbox_inches='tight')
